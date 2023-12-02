a0f17573aedfb71665a40290e5a60c8124ba48d8
sameh issa <s11925968@gmail.com>
2023-12-02 15:50:18 +0200

    icone

--- src/compount/web/home/Home.jsx
+++ src/compount/web/home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 import { Link } from 'react-router-dom'
 import { faBehance, faDribbble, faFacebook, faGoogle, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHouse, faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
 export default function Home() {
   return (
     <div>
@@ -25,19 +25,81 @@ export default function Home() {
               <Link to="/Contact/create" className="text-decoration-none">
                 create account
               </Link>
-              
             </div>
             <div className="icons">
-            <FontAwesomeIcon icon={faFacebook} className='brand brand-face'/>
-            <FontAwesomeIcon icon={faDribbble} className='brand brand-dribble'/>
-            <FontAwesomeIcon icon={faBehance}  className='brand brand-behance'/>
-            <FontAwesomeIcon icon={faLinkedinIn} className='brand brand-linked'/>
-            <FontAwesomeIcon icon={faGoogle}   className='brand brand-google'/>
-            
-              </div>
+              <FontAwesomeIcon icon={faFacebook} className="brand brand-face" />
+              <FontAwesomeIcon
+                icon={faDribbble}
+                className="brand brand-dribble"
+              />
+              <FontAwesomeIcon
+                icon={faBehance}
+                className="brand brand-behance"
+              />
+              <FontAwesomeIcon
+                icon={faLinkedinIn}
+                className="brand brand-linked"
+              />
+              <FontAwesomeIcon icon={faGoogle} className="brand brand-google" />
+            </div>
           </div>
         </div>
       </header>
+      <section className="map position-relative">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27002.02680188146!2d35.22707967947037!3d32.22434418493328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151ce0f650425697%3A0x7f0ba930bd153d84!2sNablus!5e0!3m2!1sen!2s!4v1701524940071!5m2!1sen!2s"
+          width="100%"
+          height={650}
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+        <div className="info-map position-absolute bottom-0 w-100">
+          <div className="container text-white rounded-top-3 p-3 py-4">
+            <div className="row">
+              <div className="col-lg-4">
+                <div className="info d-flex align-items-center">
+                  <FontAwesomeIcon
+                    icon={faPhone}
+                    className="solid fs-4 d-flex justify-content-center align-items-center main-color pe-2"
+                  />
+                  <span>
+                    Contact Number <br />
+                    +972 595597630
+                  </span>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="info d-flex align-items-center">
+                  <FontAwesomeIcon
+                    icon={faEnvelope}
+                    className="solid fs-4 d-flex justify-content-center align-items-center main-color pe-2"
+                  />
+                  <span className="">
+                    Email Address
+                    <br />
+                    tourplusecompany@gmail.com
+                  </span>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="info d-flex align-items-center">
+                  <FontAwesomeIcon
+                    icon={faLocationDot}
+                    className="solid fs-4 d-flex justify-content-center align-items-center main-color pe-2"
+                  />
+                  <span className="">
+                    Location
+                    <br />
+                    nablues
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
